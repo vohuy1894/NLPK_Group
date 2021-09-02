7d5e847ddab27f16f1680cb5357ca0ab164330d0
vohuy1894 <vohuy1894@gmail.com>
2021-09-02 17:11:31 -0500

    First Assignment

---
+++ NLTK1.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import matplotlib
+import urllib.request
+
+import nltk
+
+from nltk.corpus import stopwords
+
+response = urllib.request.urlopen('https://en.wikipedia.org/wiki/SpaceX')
+
+html = response.read()
+
+soup = BeautifulSoup(html, "html5lib")
+
+text = soup.get_text(strip=True)
+
+tokens = [t for t in text.split()]
+
+clean_tokens = tokens[:]
+
+sr = stopwords.words('english')
+
+# Remove stopwords from file
+for token in tokens:
+    if token in sr:
+        clean_tokens.remove(token)
+
+# Add frequency to dictionary
+freq = nltk.FreqDist(clean_tokens)
+
+# Remove value of a key less than 5
+for key, values in list(freq.items()):
+    if values < 5:
+        del freq[key]
+
+# Sorting the frequency item
+sorted(freq.items(), key=lambda item: item[1], reverse=True)  # sorting the high frequency words
+
+# print top 10 distribution words
+freq.plot(10, cumulative=False)  # print 10 high frequency keywords
